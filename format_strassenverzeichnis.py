8f0c078d2765071a8385bd1f41a3d52d4c5d9137
Hermann Schwarting <hfs@gmx.de>
2019-08-26 23:02:33 +0200

    Script to extract polling stations and streets

---
+++ format_strassenverzeichnis.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+import csv
+import re
+
+with open('tabula-strassenverzeichnis_wahllokale_wb_wk.tsv', 'r') as infile:
+    with open('wahllokale.csv', 'w') as outfile_p:
+        with open('strassenverzeichnis.csv', 'w') as outfile_s:
+            csvreader = csv.reader(infile, delimiter='\t', quotechar='"')
+            csvwriter_p = csv.writer(outfile_p, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            csvwriter_s = csv.writer(outfile_s, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+            # Output file headers
+            csvwriter_p.writerow(['Wahlkreis', 'Wahlbezirk', 'Name', 'Straße', 'PLZ+Ort', 'barrierefrei'])
+            csvwriter_s.writerow(['Wahlbezirk', 'Straße', 'Hausnummern'])
+
+            inside_polling_station = False
+            block_line = 0
+            station_attributes = []
+            ward_id = ''
+            district_id = ''
+            street = ''
+            housenumbers = ''
+
+            for row in csvreader:
+                if inside_polling_station:
+                    block_line += 1
+                    station_attributes.append(row[0])
+                    # Block always 3 lines
+                    if block_line == 3:
+                        # Some rows are smashed together
+                        match = re.match('(.*) barrierefrei', row[0])
+                        if match:
+                            del(station_attributes[-1])
+                            station_attributes.append(match.group(1))
+                            station_attributes.append('barrierefrei')
+                        elif row[1] == 'barrierefrei':
+                            station_attributes.append(row[1])
+                        else:
+                            station_attributes.append('')
+                        inside_polling_station = False
+                        block_line = 0
+                        csvwriter_p.writerow(station_attributes)
+                        station_attributes = []
+
+                # Detect Wahllokal block
+                elif row[0].startswith('Wahlbezirk'):
+                    # Some rows are smashed together
+                    match = re.match('Wahlbezirk (\d+) WK (\d+)', row[0])
+                    if match:
+                        district_id = match.group(1)
+                        ward_id = match.group(2)
+                    else:
+                        match = re.match('Wahlbezirk (\d+)', row[0])
+                        district_id = match.group(1)
+                        match = re.match('WK (\d+)', row[1])
+                        ward_id = match.group(1)
+                    inside_polling_station = True
+                    block_line = 0
+                    station_attributes = [ward_id, district_id]
+                elif ''.join(row).strip():
+                    if row[0].strip():
+                        # Output previous street, start new street
+                        if street:
+                            street_row = [district_id, street, housenumbers]
+                            csvwriter_s.writerow(street_row)
+                        street, housenumbers = row
+                    else:
+                        # Continue street with another set of housenumbers
+                        housenumbers += ', ' + row[1]
+            # Output last street
+            street_row = [district_id, street, housenumbers]
+            csvwriter_s.writerow(street_row)
